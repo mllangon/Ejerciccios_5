a59ce39ef9af7cd795e85b9d0b96a78f0b8cac8b
mllangon <mllangon@myuax.com>
2024-04-19 11:54:41 +0200

    11

--- src/main/java/Ejer_9.java
+++ src/main/java/Ejer_9.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Ejer_9 {
     public static void main(String[] args) throws IOException {
         System.out.println("Abriendo el archivo input.txt...");
-        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
         ArrayList<String> lines = new ArrayList<>();
         String line;
         System.out.println("Leyendo líneas del archivo...");
@@ -18,7 +18,7 @@ public class Ejer_9 {
         Collections.sort(lines);
 
         System.out.println("Escribiendo las líneas ordenadas en sorted_output.txt...");
-        BufferedWriter writer = new BufferedWriter(new FileWriter("sorted_output.txt"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/sorted_output.txt"));
         for (String sortedLine : lines) {
             writer.write(sortedLine + "\n");
         }
