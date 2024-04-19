8c44ae910db4cbe9de0abca14e54be9f108db5e6
mllangon <mllangon@myuax.com>
2024-04-19 11:52:52 +0200

    10

---
+++ src/main/java/Ejer_9.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Ejer_9 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Abriendo el archivo input.txt...");
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        System.out.println("Leyendo líneas del archivo...");
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        System.out.println("Archivo cerrado.");
+
+        System.out.println("Ordenando las líneas...");
+        Collections.sort(lines);
+
+        System.out.println("Escribiendo las líneas ordenadas en sorted_output.txt...");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("sorted_output.txt"));
+        for (String sortedLine : lines) {
+            writer.write(sortedLine + "\n");
+        }
+        writer.close();
+        System.out.println("Archivo sorted_output.txt cerrado.");
+    }
+}
